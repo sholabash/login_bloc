347555f8c4d1d65a51e577b6ef6d12dbcaebab9c
sholabash <sholabash106@gmail.com>
2022-12-31 04:19:46 +0100

    mm

--- lib/blocs/provider.dart
+++ lib/blocs/provider.dart
@@ -4,15 +4,20 @@ import 'bloc.dart';
 //instead of using instance this scope is an alternative
 
 class Provider extends InheritedWidget {
+  Provider({super.key, required super.child});
+
   final bloc = Bloc();
 
-  Provider({Key? key, required Widget child}) : super(key: key, child: child);
-
-  bool updateShouldNotify(_) => true;
-
-  static Bloc of(BuildContext context) {
-    return (context.dependOnInheritedWidgetOfExactType(aspect: Provider)
-            as Provider)
-        .bloc;
+  static Provider? maybeOf(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>() as Provider;
   }
+
+  static Provider of(BuildContext context) {
+    final Provider? result = maybeOf(context);
+    assert(result != null, 'No FrogColor found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(Provider oldWidget) => true;
 }
